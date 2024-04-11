42a1c9fc528b24ad2f18fdccfbb65740c68a2b2d
Diniyar <d.zhagparov@galamat.biz>
2024-04-11 16:40:34 +0500

    Add WebSocket endpoint for sending reminders in server/main.go

--- server/main.go
+++ server/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"time"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"golang.org/x/net/websocket"
 )
 
 func main() {
@@ -16,6 +19,7 @@ func main() {
 	e.Use(middleware.KeyAuth(func(key string, c echo.Context) (bool, error) {
 		return key == "123", nil
 	}))
+	e.GET("/ws", sendRemind)
 
 	e.GET("/", func(c echo.Context) error {
 		data := GetNotes()
@@ -71,4 +75,45 @@ func main() {
 	})
 
 	e.Logger.Fatal(e.Start(":8000"))
+}
+
+func sendRemind(c echo.Context) error {
+	websocket.Handler(func(ws *websocket.Conn) {
+		defer ws.Close()
+		for {
+			// Write
+			err := websocket.Message.Send(ws, "Hello, Client!")
+			if err != nil {
+				c.Logger().Error(err)
+			}
+
+			go CheckRemind(ws)
+
+			// Read
+			msg := ""
+			err = websocket.Message.Receive(ws, &msg)
+			if err != nil {
+				c.Logger().Error(err)
+			}
+			fmt.Printf("%s\n", msg)
+		}
+	}).ServeHTTP(c.Response(), c.Request())
+	return nil
+}
+
+func CheckRemind(ws *websocket.Conn) {
+	for {
+		notes := GetNotes()
+		for _, note := range notes {
+			if note.RemindDate == time.Now().String() {
+				sendNote, err := json.Marshal(note)
+				if err != nil {
+					fmt.Println(err)
+				} else {
+					websocket.Message.Send(ws, string(sendNote))
+				}
+			}
+		}
+		time.Sleep(1 * time.Minute)
+	}
 }
