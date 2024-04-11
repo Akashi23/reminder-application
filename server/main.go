4b46162f9484891445528b005b9e10ca8e77c6c7
Diniyar <d.zhagparov@galamat.biz>
2024-04-11 15:36:31 +0500

    Update server code to use echo middleware for logging and error recovery

--- server/main.go
+++ server/main.go
@@ -4,10 +4,14 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
 
 	e.GET("/", func(c echo.Context) error {
 		data := GetNotes()
